c0fa0894d2e4c471452b92b1ef2987e9811b6324
Demon000 <demonsingur@gmail.com>
2019-08-12 06:11:58 +0200

    vayu: Build opensource display stack
    Author: Tharaga Balachandran <tbalacha@codeaurora.org>
    Date:   Thu Sep 26 19:17:58 2019 -0400
    
        vayu: gralloc: Upgrade mapper and allocators to v3.0
    
        CRs-Fixed: 2445393
        Change-Id: I8c44ab63ca81523bb6acb75e8ae878cc7466e139
    
    Author: Tharaga Balachandran <tbalacha@codeaurora.org>
    Date:   Wed Jan 8 17:17:56 2020 -0500
    
        vayu: gralloc: Add support for IMapper/IAllocator 4.0
    
        * Add QtiMapper and QtiAllocator 4.0
        * Implement IMapper 4.0 APIs: get, set,
          getFromBufferDescriptorInfo, flushLockedBuffer,
          rereadLockedBuffer, listSupportedMetadataTypes,
          dumpBuffer, dumpBuffers, and getReservedRegion
    
        CRs-Fixed: 2633272
        Change-Id: Ica42e485e426b366ffb41f30f1ea2cf395901356
        Signed-off-by: Baldev Sahu <bsahu@codeaurora.org>
    
    Change-Id: I4ae143d0cc0fba379894e1f69b793312192f0c4a

--- device.mk
+++ device.mk
@@ -109,6 +109,23 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.4-service \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
+    vendor.qti.hardware.display.allocator-service
+
+PRODUCT_PACKAGES += \
+    gralloc.msmnile \
+    hwcomposer.msmnile \
+    memtrack.msmnile
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
+
 # Fstab
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom \
@@ -137,6 +154,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml
+
+# Touchscreen
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
 # Include vendor
 include vendor/xiaomi/vayu/vayu-vendor.mk
