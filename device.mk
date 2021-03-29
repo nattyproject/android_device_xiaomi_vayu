c4fe3656a3b0a6ebead1566277810bee03e102d2
daniml3 <danimoral1001@gmail.com>
2021-03-29 15:58:12 +0200

    vayu: build fastbootd
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I14776da68eb2cf3e4a1a0c06da70f53a054da784

--- device.mk
+++ device.mk
@@ -126,6 +126,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Fstab
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom \
