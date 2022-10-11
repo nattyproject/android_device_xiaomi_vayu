ee9b2876eac334b96ee6759ca26d0f7afbd64fe4
Ghanish <shghanish@proton.me>
2022-10-11 19:56:51 +0530

    vayu: Configure Telephony
    - Build telephony packages
    - Import Cne, IMS, DPM and Ril blobs from stock
    - Copy permissions
    
    Change-Id: Id50ef4cd07670e6932a1a6d6e3c380db3a3972eb

--- device.mk
+++ device.mk
@@ -32,5 +32,25 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
+# IMS & Telephony
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-hidl-wrapper-prd \
+    qti_telephony_hidl_wrapper-prd.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml
+
 # Include vendor
 include vendor/xiaomi/vayu/vayu-vendor.mk
