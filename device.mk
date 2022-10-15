1bae325f7eaa9f3933d2bbe45eb73ea44862ed36
Ghanish <shghanish@proton.me>
2022-10-15 11:51:50 +0530

    vayu: Build init scripts
    Change-Id: Ic6efb2fc0d863372bcb030c2527e7c5c69fc9162

--- device.mk
+++ device.mk
@@ -147,8 +147,7 @@ PRODUCT_PACKAGES += \
 
 # Fstab
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
 
 # Fingerprint
 PRODUCT_PACKAGES += \
@@ -162,6 +161,24 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
+
+# Init scritps
+PRODUCT_COPY_FILES += \
+    init.mi.usb.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.class_core.sh \
+    init.qcom.sensors.sh \
+    init.qcom.sh \
+    init.qcom.usb.sh \
+    init.qti.dcvs.sh
+
+PRODUCT_COPY_FILES += \
+    fstab.qcom \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.target.rc \
+    ueventd.rc
 
 # Lights
 PRODUCT_PACKAGES += \
